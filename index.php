e2d31ae5439b9700d1f83ce9ee1ed655ae616430
Nana Yeboah <yeboahnanaosei@gmail.com>
2018-02-18 12:51:53 +0000

    Specify image types in accept attribute

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <body>
 <form id="image-form" method="post" enctype="multipart/form-data" action="image-uploader.php">
     <h3>Upload an image</h3>
-    <input type="file" name="pic" accept="image/*">
+    <input type="file" name="pic" accept=".jpg, .png, .jpeg, .gif">
     <input type="submit" value="Upload" name="upload">
 </form>
 </body>
